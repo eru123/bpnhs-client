c121b4c9bd1f0edc787e6a0cf5881aa6b8b1f4a1
eru123 <yeoligoakino@gmail.com>
2020-12-23 00:30:26 +0800

    Added login and register

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,39 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Home from "@/views/Home.vue";
+import About from "@/views/About.vue";
+import Login from "@/views/Login.vue";
+import Register from "@/views/Register.vue";
+import NonAuth from "@/views/NonAuth.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path: "/",
+    path: "/home",
     name: "Home",
     component: Home
   },
   {
     path: "/about",
     name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    component: About
+  },
+  {
+    path: "/login",
+    component: NonAuth,
+    children: [
+      {
+        path: "",
+        name: "Login",
+        component: Login
+      },
+      {
+        path: "/register",
+        name: "Register",
+        component: Register
+      }
+    ]
   }
 ];
 
---
+++ src/views/NonAuth.vue
@@ -0,0 +1,65 @@
+  
+<template>
+  <v-app id="inspire">
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+    >   
+        <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="title">
+            Application
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            Menu
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list
+        dense
+        nav
+      >
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          link
+          :to="item.path"
+          color="primary"
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app color="primary" dark outlined elevation="0">
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>Authenticate</v-toolbar-title>
+    </v-app-bar>
+    <v-main color="#333">
+      <router-view></router-view>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data: () => ({ 
+        drawer: false ,
+        items: [
+          { title: 'Home', icon: 'mdi-home', path: {name:"Home"}},
+          { title: 'Login', icon: 'mdi-home', path: {name:"Login"}},
+          { title: 'Register', icon: 'mdi-home',path: {name: "Register"}},
+        ],
+        right: null,
+    }),
+  }
+</script>
