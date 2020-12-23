d7bfddd9058dc1385cdb04327882245e53289d1e
eru123 <yeoligoakino@gmail.com>
2020-12-23 14:42:07 +0800

    Added plugins

---
+++ src/plugins/db/index.js
@@ -0,0 +1,4 @@
+const token = require('./token');
+
+
+module.exports = {token};
---
+++ src/plugins/db/token.js
@@ -0,0 +1,27 @@
+const store = require('store');
+
+const tokenKey = '__bpnhs_token';
+
+const test = (token) => {
+    store.set(set,token);
+};
+const isStored = () => {
+    let token = store.get(tokenKey);
+    try {
+        if(typeof token === "string" && token.length > 0){
+            return true;
+        }
+    } catch (e) {
+        return false;
+    }
+
+    return false;
+}
+const set = (token) => {
+    store.set(tokenKey,token);
+};
+const get = () => {
+    return store.get(tokenKey);
+};
+
+module.exports = {set,get,test,isStored};
