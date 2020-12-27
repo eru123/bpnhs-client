c42d540789c957e8cda150245d8415967cd3e475
Jericho Aquino <yeoligoakino@gmail.com>
2020-12-27 17:50:21 +0800

    Added alert component

---
+++ src/components/Alert.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-row justify="center">
+    <v-dialog v-model="show" persistent max-width="290">
+      <v-card>
+        <v-card-title class="headline">
+          {{ title }}
+        </v-card-title>
+        <v-card-text> {{ message }} </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="blue darken-1" text @click="close"> Close </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-row>
+</template>
+<script>
+export default {
+  name: "ErrorPrompt",
+  methods: {
+    close() {
+      this.$emit("close", false);
+    },
+  },
+  props: {
+    title: String,
+    show: Boolean,
+    message: String,
+  },
+};
+</script>
