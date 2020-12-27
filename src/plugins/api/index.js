e4004943df51285033869e4b56cef5d5917217b2
Jericho Aquino <yeoligoakino@gmail.com>
2020-12-27 17:50:36 +0800

    change default host

--- src/plugins/api/index.js
+++ src/plugins/api/index.js
@@ -1,7 +1,7 @@
 const api = require("./api");
 
 // Change default
-const default_host = "http://localhost:8080/api.php";
+const default_host = "api.php";
 
 const host = process.env.VUE_APP_API_SERVER || default_host;
 console.log(host);
